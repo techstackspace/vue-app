c51a3d37dfd1137c37d91b8449cbb31c41fc3c79
Osagie Bello <info@techstackspace.com>
2025-10-15 14:32:50 +0100

    first commit

---
+++ public/index.js
@@ -0,0 +1,41 @@
+// Composition API
+const app = Vue.createApp({
+  setup() {
+    const message = Vue.ref('This is a new message')
+    const count = Vue.ref(0)
+    const clickMe = () => {
+      console.log('This button was clicked')
+      message.value = 'Updated message'
+    }
+    const increment = () => {
+      count.value++
+    }
+    return {
+      message,
+      clickMe,
+      count,
+      increment,
+    }
+  },
+})
+
+app.mount('#app')
+
+// Options Method
+// const app = Vue.createApp({
+//     data() {
+//         return {
+//             message: 'This is a new message',
+//             count: 0
+//         };
+//     },
+//     methods: {
+//         clickMe() {
+//             console.log('This button was clicked');
+//             this.message = 'Updated message';
+//         },
+//         increment() {
+//             this.count++;
+//         }
+//     }
+// });
---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+interface User {
+  status: string
+  name: string
+  age: number
+  isMarried: boolean
+  link: string
+}
+const userInfo = {
+  status: 'pending',
+  name: 'John',
+  age: 30,
+  isMarried: false,
+  link: 'https://google.com',
+} as User
+const skills = ['HTML', 'CSS', 'JavaScript', 'Vue']
+const user = userInfo.name
+</script>
+
+<template>
+  <div>
+    <h2>User Info</h2>
+    <ul>
+      <li>{{ user }} is {{ userInfo.age }} years old</li>
+      <li v-if="userInfo.isMarried">{{ user }} is married</li>
+      <li v-else>{{ user }} is not married</li>
+      <li v-if="userInfo.status === 'pending'">{{ user }} request is pending</li>
+      <li v-else-if="userInfo.status === 'active'">{{ user }} request is accepted</li>
+      <li v-else>{{ user }} request is rejected</li>
+      <li>Learn more from <a :href="userInfo.link">user website</a></li>
+    </ul>
+
+    <h2>User Tasks</h2>
+    <ul>
+      <li v-for="skill in skills" :key="skill">{{ skill }}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
